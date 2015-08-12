190191ad27c70e181e1d40039fa70907f343258e
Ivo Jimenez <ivo.jimenez@gmail.com>
2015-08-12 04:13:38 -0700

    Fixes main so it prints usage() correctly
    Also, log.Fatalln() is used instead of log.Fatal()

--- cmd/aver/main.go
+++ cmd/aver/main.go
@@ -16,33 +16,36 @@ var fileType string
 func main() {
 
 	var cmd = &cobra.Command{
-		Use:   "aver [OPTIONS] \"<statement(s)>\"",
+		Use:   "aver \"<statement(s)>\"",
 		Short: "Aver helps you automatically validate assertions on data",
 		Long:  ``,
 		Run:   Execute,
 	}
 
-	cmd.Flags().StringVarP(&dbConfig, "dbconf", "c", "", `Name of file
-			containing database configuration. Format is JSON where only top-level
-			elements are considered. See http://github.com/ivotron/aver for supported
-			drivers and configuration examples.`)
+	cmd.Flags().StringVarP(&dbConfig, "dbconf", "c", "", `Name of file containing
+			database configuration. Format is JSON where only top-level elements are
+			considered. See http://github.com/ivotron/aver for supported drivers and
+			configuration examples.`)
 	cmd.Flags().StringVarP(&dataFile, "input", "i", "", `File to read data from.
-			Format is inferred from file extension ('csv' and 'json' supported). 'csv'
-			is assumed for files without extension`)
+			Format is inferred from file extension. 'csv' and 'json' supported; 'csv'
+			is assumed for files without extension.`)
 
 	cmd.Execute()
 }
 
 func Execute(cmd *cobra.Command, args []string) {
-	if dataFile != "" && dbConfig != "" {
-		log.Fatal("ERROR: Only one two options 'dbconf' and 'input' allowed.")
+	if len(args) == 0 {
+		log.Fatalln(cmd.UsageString())
 	}
-	if len(args) > 1 {
-		log.Fatal("ERROR: Expecting one double-quoted string as argument.")
+	if dataFile != "" && dbConfig != "" {
+		log.Fatalln("ERROR: Options 'dbconf' and 'input' cannot be used simultaneously.")
+	}
+	if len(args) != 1 {
+		log.Fatalln("ERROR: Expecting one double-quoted string as argument.")
 	}
 	if strings.HasSuffix(dataFile, ".json") {
 		fileType = "json"
-		log.Fatal("ERROR: JSON not supported yet.")
+		log.Fatalln("ERROR: JSON not supported yet.")
 	} else {
 		fileType = "csv"
 	}
@@ -54,12 +57,12 @@ func Execute(cmd *cobra.Command, args []string) {
 
 	db, tblName, err := aver.MakeDb(dataFile, fileType)
 	if err != nil {
-		log.Fatal("ERROR: " + err.Error())
+		log.Fatalln("ERROR: " + err.Error())
 	}
 
 	holds, err := aver.Holds(args[0], db, tblName)
 	if err != nil {
-		log.Fatal("ERROR: " + err.Error())
+		log.Fatalln("ERROR: " + err.Error())
 	}
 
 	db.Close()
